52d3896a57fa0340bd342b37cc7c1e3b671ac667
jakub-galecki <70712523+jakub-galecki@users.noreply.github.com>
2024-02-12 23:21:27 +0100

    Update remap.lua

--- lua/usr/remap.lua
+++ lua/usr/remap.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>gg", vim.cmd.GitGutterToggle)
+vim.keymap.set("n", "<leader>ggt", vim.cmd.GitGutterToggle)
 vim.keymap.set("n", "<leader>ggb", vim.cmd.BlamerToggle)
-
+vim.keymap.set("n", "]h", vim.cmd.GitGutterNextHunk)
+vim.keymap.set("n", "[h", vim.cmd.GitGutterPrevHunk)
+vim.keymap.set("n", "<leader>ggs", vim.cmd.GitGutterPreviewHunk)
 
