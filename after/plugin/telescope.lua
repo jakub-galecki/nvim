737dfff386e24d8eb39d1238761947ba4481a90d
jakub-galecki <70712523+jakub-galecki@users.noreply.github.com>
2024-12-10 22:17:50 +0100

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_status, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({search = vim.fn.input("Grep > ")});
 end)
