5c87f1716d2a8e37e5c4c87e0dc6f54603bc911b
jakub-galecki <324384@uwr.edu.pl>
2025-01-10 00:04:52 +0100

    harpoon

--- lua/usr/packer.lua
+++ lua/usr/packer.lua
@@ -90,4 +90,11 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+    use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
 end)
